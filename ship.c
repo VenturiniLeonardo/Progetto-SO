7d1c11c97c69ab5f433671500a42281e8f89502d
VittuD <davide.vitturini@edu.unito.it>
2022-12-02 11:34:13 +0100

    update of randGen functions in ship.c and master.c

--- ship.c
+++ ship.c
@@ -15,9 +15,9 @@
 struct coords generateRandCoords(){
     struct coords c;
     srand(time(0));
-    double dec=drand48();
+    double dec=rand()/(RAND_MAX*1.0);
     c.x = dec*SO_LATO;
-    dec=drand48();
+    dec=rand()/(RAND_MAX*1.0);
     c.y = dec*SO_LATO;
     return c;
     
